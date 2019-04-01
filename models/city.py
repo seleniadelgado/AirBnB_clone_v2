03264ef3ede5b813833d0b616491b39d0ec06084
seleniadelgado <angiedelgado08@gmail.com>
2019-04-01 22:21:52 +0000

    Updating city class and tests with minor fixes and making tests work

--- models/city.py
+++ models/city.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy import ForeignKey
+
 
 class City(BaseModel, Base):
     """This is the class for City
@@ -13,6 +14,5 @@ class City(BaseModel, Base):
     state_id = ""
     name = ""
     __tablename__ = "cities"
-    name = Column(string(128), nullable=False),
-    state_id = Column(string(60), ForeignKey("states.id"), nullable=False)
-    """"""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
