2e403096d857e229ae8e3d941f439a86e1a90cc5
--get <angiedelgado08@gmail.com>
2019-04-01 06:32:58 +0000

    city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
@@ -11,3 +12,7 @@ class City(BaseModel):
     """
     state_id = ""
     name = ""
+    __tablename__ = "cities"
+    name = Column(string(128), nullable=False),
+    state_id = Column(string(60), ForeignKey("states.id"), nullable=False)
+    """"""
