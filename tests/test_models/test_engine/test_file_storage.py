dfbaf7e12054c42a19c0377b395a9de32859ec1b
seleniadelgado <angiedelgado08@gmail.com>
2019-04-01 00:19:21 +0000

    Updating tests for file storage delete and all to be more accurate and updating filestorage with delete and better all method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -57,7 +57,7 @@ class TestFileStorage(unittest.TestCase):
         storage.new(state)
         stAll = storage.all(State)
         self.assertIn("State.{}".format(state.id), stAll.keys())
-        self.asserNotIn("User.{}".format(user.id), stAll.keys())
+        self.assertNotIn("User.{}".format(user.id), stAll.keys())
 
     def test_new(self):
         """test when new is created"""
