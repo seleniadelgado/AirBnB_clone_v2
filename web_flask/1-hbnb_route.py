dd9de0576e4db2961052df74e0047b4da2656f56
--get <angiedelgado08@gmail.com>
2019-04-23 02:08:47 +0000

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+strict_slashes = False
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+@app.route("/hbnb")
+def hbnb():
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
